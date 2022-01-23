b2ef9a7498d3840966d25b9f6ec4fa013276020c
kekosh <kenta0540@gmail.com>
2022-01-23 23:38:32 +0900

    i18n対応実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    locale = 'en' if ['en', 'ja'].include?(locale) == false
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,6 @@
 
 class Book < ApplicationRecord
   mount_uploader :picture, PictureUploader
+
+  validates :title, :memo, :author, presence: true
 end
