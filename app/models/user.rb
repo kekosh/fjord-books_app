2ce524c5da6c7763d0553bf3081c430d843114a5
kekosh <kenta0540@gmail.com>
2022-06-16 21:34:15 +0900

    アイコンを任意で登録できるよう修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :icon
-  validates :icon, attached: true, content_type: %i[jpg png gif]
+  validates :icon, content_type: %i[jpg png gif]
 end
