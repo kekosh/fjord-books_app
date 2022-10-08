a0eac5ec80ac7ee23bf3c09cb67fc5c50dcd61b7
kenta koshio <kekosh@MacBook.local>
2022-10-08 16:31:12 +0900

    rubocopチェック結果を修正

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,29 +3,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "should_return_email-address_if_name_nothing" do
-    user = User.new(name: '', email:'foo@example.com')
+  test 'should_return_email-address_if_name_nothing' do
+    user = User.new(name: '', email: 'foo@example.com')
     assert_equal('foo@example.com', user.name_or_email)
   end
 
-  test "shold_return_name_if_name_filed" do
-    user = User.new(name: "Bar bar", email: "bar@example.com")
+  test 'shold_return_name_if_name_filed' do
+    user = User.new(name: 'Bar bar', email: 'bar@example.com')
     assert_equal('Bar bar', user.name_or_email)
   end
 
-  test "should_can_follow_other_user" do
-    follower = User.create(name:"Alice", email:"alice@example.com", password:"password")
-    followed = User.create(name:"Bob", email:"bob@example.com", password:"password")
+  test 'should_can_follow_other_user' do
+    follower = User.create(name: 'Alice', email: 'alice@example.com', password: 'password')
+    followed = User.create(name: 'Bob', email: 'bob@example.com', password: 'password')
 
     assert_not follower.following?(followed)
     follower.follow(followed)
     assert follower.following?(followed)
   end
 
-  test "should_can_show_followed_users" do
-    followed_alice = User.create(name:"Alice", email:"alice@example.com", password:"password")
-    follower_bob = User.create(name:"Bob", email:"bob@example.com", password:"password")
-    follower_carol = User.create(name:"Carol", email:"carol@example.com", password:"password")
+  test 'should_can_show_followed_users' do
+    followed_alice = User.create(name: 'Alice', email: 'alice@example.com', password: 'password')
+    follower_bob = User.create(name: 'Bob', email: 'bob@example.com', password: 'password')
+    follower_carol = User.create(name: 'Carol', email: 'carol@example.com', password: 'password')
 
     assert_not followed_alice.followed_by?(follower_bob)
     assert_not followed_alice.followed_by?(follower_carol)
@@ -35,9 +35,9 @@ class UserTest < ActiveSupport::TestCase
     assert followed_alice.followed_by?(follower_carol)
   end
 
-  test "follower_can_unfollow" do
-    follower = User.create(name:"Alice", email:"alice@example.com", password:"password")
-    followed = User.create(name:"Bob", email:"bob@example.com", password:"password")
+  test 'follower_can_unfollow' do
+    follower = User.create(name: 'Alice', email: 'alice@example.com', password: 'password')
+    followed = User.create(name: 'Bob', email: 'bob@example.com', password: 'password')
     follower.follow(followed)
     assert followed.followed_by?(follower)
 
